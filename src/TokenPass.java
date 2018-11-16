16a99905b027122c7251f37a2cc09f3cabcc847d
BT_1N3_11 <cyang4980@bths.edu>
2018-11-16 10:17:09 -0500

    asdsd

--- src/TokenPass.java
+++ src/TokenPass.java
@@ -6,14 +6,17 @@ public class TokenPass {
 
     public TokenPass(int playerCount) {
         board = new int[playerCount];
-        for (int i = 0; i < playerCount; i++)
+        for (int i = 0; i < playerCount; i++) {
             board[i] = (int) (10 * Math.random()) + 1;
+        }
         currentPlayer = (int) (playerCount * Math.random());
+        System.out.println("Current Player " + currentPlayer);
     }
 
 
     public void distributeCurrentPlayerTokens() {
         int numTokens = board[currentPlayer];
+        System.out.println("numTokens : " + numTokens);
         board[currentPlayer] = 0;
         int i = currentPlayer;
         while (numTokens > 0) {
@@ -24,14 +27,29 @@ public class TokenPass {
     }
 
     public void printBoard() {
-        System.out.println(Arrays.toString(board));
+        //System.out.println(Arrays.toString(board));
+
+        String display = "[ ";
+        for (int i=0;i<board.length;i++)
+        {
+            display += board[i] + ", ";
+        }
+        display += "]";
+        System.out.println(display);
     }
 
     public void nextPlayer() {
-        currentPlayer++;
+        currentPlayer = (currentPlayer + 1) % board.length;
     }
 
     public int gameOver() {
-        return java.util.Arrays.asList(board).indexOf(0);
+        for (int i = 0; i<board.length; i++)
+        {
+            if (board[i] == 0)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
